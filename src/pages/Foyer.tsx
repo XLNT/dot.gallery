dc0d020c22415a29b77e8792087d5353a10114e0
Matt Condon (shrugs) <mattgcondon@gmail.com>
2019-08-25 04:32:32 +0200

    feat: foyer

--- src/pages/Foyer.tsx
+++ src/pages/Foyer.tsx
@@ -1,21 +1,48 @@
 import { ExhibitionProps, Flow } from "./ExhibitionProps";
 import { preloadImage } from "lib/preload";
-import PanelContent from "context/PanelContent";
-import React, { useEffect } from "react";
-import WithContentTransition from "components/WithContentTransition";
+import React, { useCallback, useEffect } from "react";
+import arrow from "components/grey_arrow.svg";
+import foyerMp4 from "files/foyer.mp4";
+import fromTheme from "theme/fromTheme";
 import styled from "styled-components";
 import useCurrentExhibition from "hook/useCurrentExhibition";
 import useEnforcePanelVisibility from "hook/useEnforcePanelVisibility";
 import useSuggestedPanelState from "hook/useSuggestedPanelState";
 
-const Container = styled.div`
+const Video = styled.video`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const SkipButton = styled.div`
+  position: absolute;
+  bottom: 2rem;
+  right: 2rem;
+  color: ${fromTheme("background")};
+  font-weight: bold;
+  cursor: pointer;
   display: flex;
-  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Arrow = styled.img`
+  margin-left: 0.5rem;
+  width: 1rem;
+  height: 1rem;
+  transform: rotate(180deg);
 `;
 
 export default function Foyer({ setFlow }: ExhibitionProps<void>) {
   useEnforcePanelVisibility(false);
   useSuggestedPanelState(false);
+
+  const goGallery = useCallback(() => setFlow(Flow.Gallery), [setFlow]);
+
   const { exhibition } = useCurrentExhibition();
   useEffect(() => {
     if (exhibition) {
@@ -25,12 +52,13 @@ export default function Foyer({ setFlow }: ExhibitionProps<void>) {
   }, [exhibition]);
 
   return (
-    <Container>
-      <div>Foyer</div>
-      <button onClick={() => setFlow(Flow.Gallery)}>next</button>
-      <PanelContent.Source>
-        <WithContentTransition>Test</WithContentTransition>
-      </PanelContent.Source>
-    </Container>
+    <>
+      <Video autoPlay onEnded={goGallery}>
+        <source src={foyerMp4} type="video/mp4" />
+      </Video>
+      <SkipButton onClick={goGallery}>
+        Skip <Arrow src={arrow} />
+      </SkipButton>
+    </>
   );
 }
