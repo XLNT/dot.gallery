e15399affb5278228c119c6647ec08e167dbfdc6
Matt Condon (shrugs) <mattgcondon@gmail.com>
2019-09-07 18:07:35 +0200

    fix: make env available in build

--- packages/gallery/src/config.ts
+++ packages/gallery/src/config.ts
@@ -1,10 +1,10 @@
 // env-related config information
 
 const config = {
+  BASE_URI: process.env.BASE_URI,
   BACKROOM_URI: process.env.BACKROOM_URI,
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   STRIPE_PUBLISHABLE: process.env.STRIPE_PUBLISHABLE,
-  BASE_URI: process.env.BASE_URI,
 };
 
 console.log(config);
