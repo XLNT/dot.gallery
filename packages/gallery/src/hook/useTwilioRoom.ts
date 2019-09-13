3f4050cdc4946a47aff93df1f18f9298064cdd79
Matt Condon (shrugs) <mattgcondon@gmail.com>
2019-09-13 15:56:56 +0200

    clean up audio logic

--- packages/gallery/src/hook/useTwilioRoom.ts
+++ packages/gallery/src/hook/useTwilioRoom.ts
@@ -6,9 +6,24 @@ import {
   Room,
   TwilioError,
   connect,
+  createLocalTracks,
 } from "twilio-video";
-import { filter, mapValues, uniq } from "lodash-es";
-import { useCallback, useEffect, useMemo, useReducer, useRef } from "react";
+import { differenceBy, mapValues, uniq, uniqBy } from "lodash-es";
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
+import update, { extend } from "immutability-helper";
+
+import usePreviousValue from "./usePreviousValue";
+
+extend("$autoArray", function(value, object) {
+  return object ? update(object, value) : update([], value);
+});
 
 enum RoomState {
   Disconnected,
@@ -38,7 +53,7 @@ type Action =
   | { type: ActionType.OnConnect; payload: { room: Room } }
   | {
       type: ActionType.OnDisconnect;
-      payload: { room: Room; error: TwilioError };
+      payload: { room?: Room; error?: TwilioError };
     }
   | { type: ActionType.OnReconnecting; payload: { error: TwilioError } }
   | { type: ActionType.OnReconnected }
@@ -116,42 +131,44 @@ const reducer = (state: State, action: Action): State => {
 
     case ActionType.OnConnect: {
       const { room } = action.payload;
-      return {
-        ...state,
-        room,
-        roomState: RoomState.Connected,
-        error: undefined,
-      };
+      console.log(`connected to room ${room.sid}`);
+
+      return update(state, {
+        room: { $set: room },
+        roomState: { $set: RoomState.Connected },
+        error: { $set: undefined },
+      });
     }
     case ActionType.OnDisconnect: {
       const { room, error } = action.payload;
       console.log(
         `[local] disconnected from room ${room.sid} with error ${error}`,
       );
-      return {
-        ...state,
-        room: undefined,
-        roomState: RoomState.Disconnected,
-        error,
-        participants: [],
-        subscribedTracks: {},
-      };
+
+      return update(state, {
+        room: { $set: undefined },
+        roomState: { $set: RoomState.Disconnected },
+        error: { $set: error },
+        participants: { $set: [] },
+        subscribedTracks: { $set: {} },
+      });
     }
     case ActionType.OnReconnecting: {
       const { error } = action.payload;
       console.log(`[local] reconnecting with error ${error}`);
-      return {
-        ...state,
-        roomState: RoomState.Reconnecting,
-        error,
-      };
+
+      return update(state, {
+        roomState: { $set: RoomState.Reconnecting },
+        error: { $set: error },
+      });
     }
     case ActionType.OnReconnected: {
-      return {
-        ...state,
-        roomState: RoomState.Connected,
-        error: undefined,
-      };
+      console.log(`REconnected to room ${state.room.sid}`);
+
+      return update(state, {
+        roomState: { $set: RoomState.Connected },
+        error: { $set: undefined },
+      });
     }
 
     // tracks
@@ -161,6 +178,7 @@ const reducer = (state: State, action: Action): State => {
       console.log(
         `participant ${participant.identity} has enabled track ${publication.trackSid}`,
       );
+
       return state;
     }
     case ActionType.OnTrackDisabled: {
@@ -168,6 +186,7 @@ const reducer = (state: State, action: Action): State => {
       console.log(
         `participant ${participant.identity} has DISenabled track ${publication.trackSid}`,
       );
+
       return state;
     }
     case ActionType.OnTrackPublished: {
@@ -179,6 +198,7 @@ const reducer = (state: State, action: Action): State => {
           publication.isSubscribed ? "are" : "are not"
         } subscribed to.`,
       );
+
       return state;
     }
     case ActionType.OnTrackUnpublished: {
@@ -190,6 +210,7 @@ const reducer = (state: State, action: Action): State => {
           publication.isSubscribed ? "are" : "are not"
         } subscribed to.`,
       );
+
       return state;
     }
     case ActionType.OnTrackSubscribed: {
@@ -197,16 +218,15 @@ const reducer = (state: State, action: Action): State => {
       console.log(
         `we have subscribed to participant ${participant.identity}'s ${track.kind} track ${track.sid}`,
       );
-      return {
-        ...state,
+
+      return update(state, {
         subscribedTracks: {
-          ...state.subscribedTracks,
-          [participant.identity]: [
-            ...(state.subscribedTracks[participant.identity] || []),
-            track,
-          ],
+          [participant.identity]: tracks =>
+            update(tracks || [], tracks =>
+              uniqBy([...tracks, track], t => t.sid),
+            ),
         },
-      };
+      });
     }
     case ActionType.OnTrackUnsubscribed: {
       const { track, participant } = action.payload;
@@ -214,17 +234,14 @@ const reducer = (state: State, action: Action): State => {
         `we have UNsubscribed to participant ${participant.identity}'s ${track.kind} track ${track.sid}`,
       );
 
-      // // detatch from track
-      // if (track.kind === "audio") {
-      //   track.detach();
-      // }
-      // remove from cache
-      return {
-        ...state,
-        subscribedTracks: mapValues(state.subscribedTracks, tracks =>
-          tracks.filter(t => t.sid !== track.sid),
-        ),
-      };
+      return update(state, {
+        subscribedTracks: {
+          [participant.identity]: tracks =>
+            update(tracks || [], tracks =>
+              tracks.filter(t => t.sid !== track.sid),
+            ),
+        },
+      });
     }
 
     // participants
@@ -233,40 +250,43 @@ const reducer = (state: State, action: Action): State => {
       const { participant } = action.payload;
       console.log(`participant ${participant.identity} joined!`);
 
-      return {
-        ...state,
-        participants: [...state.participants, participant],
-      };
+      return update(state, {
+        participants: participants =>
+          update(participants || [], participants =>
+            uniqBy([...participants, participant], i => i.identity),
+          ),
+      });
     }
     case ActionType.OnParticipantDisconnected: {
       const { participant } = action.payload;
       console.log(`participant ${participant.identity} left!`);
 
-      return {
-        ...state,
-        participants: state.participants.filter(
-          p => p.identity !== participant.identity,
-        ),
-      };
+      return update(state, {
+        participants: participants =>
+          update(participants || [], participants =>
+            participants.filter(p => p.identity !== participant.identity),
+          ),
+      });
     }
 
     // muting
 
     case ActionType.SetSelfMuted: {
       const { selfMuted } = action.payload;
-      return {
-        ...state,
-        selfMuted,
-      };
+
+      return update(state, {
+        selfMuted: { $set: selfMuted },
+      });
     }
     case ActionType.SetParticipantMuted: {
       const { id, muted } = action.payload;
-      return {
-        ...state,
-        mutedParticipantIds: muted
-          ? uniq([...state.mutedParticipantIds, id])
-          : filter(state.mutedParticipantIds, i => i !== id),
-      };
+
+      return update(state, {
+        mutedParticipantIds: ids =>
+          update(ids || [], ids =>
+            muted ? uniq([...ids, id]) : ids.filter(i => i !== id),
+          ),
+      });
     }
 
     default:
@@ -343,6 +363,7 @@ export default function useTwilioRoom(
   id: string,
   tracks: LocalTrack[],
 ) {
+  const [_room, _setRoom] = useState<Room>();
   const elementCache = useRef<{ [_: string]: HTMLAudioElement }>({});
   const [state, dispatch] = useReducer(reducer, {
     roomState: RoomState.Disconnected,
@@ -352,15 +373,14 @@ export default function useTwilioRoom(
     mutedParticipantIds: [],
   });
 
+  const prevParticipants = usePreviousValue(state.participants);
+
   const {
-    room,
     participants,
     subscribedTracks,
     selfMuted,
     mutedParticipantIds,
   } = state;
-
-  const canConnect = tracks !== null && !!id && !!token;
 
   // idempotent (afaict)
   const loadTrack = useCallback((track: RemoteTrack) => {
@@ -385,75 +405,88 @@ export default function useTwilioRoom(
     }
   }, []);
 
-  // handle room changes
+  // ensure local state knows about sync-known track publications
   useEffect(() => {
-    if (!room) return;
+    const newParticipants = differenceBy(
+      participants,
+      prevParticipants,
+      p => p.identity,
+    );
+    newParticipants.forEach(participant => {
+      participant.tracks.forEach(publication => {
+        console.log(
+          `[local] publishing known track ${publication.trackSid} for existing participant ${participant.identity}`,
+        );
+        dispatch({
+          type: ActionType.OnTrackPublished,
+          payload: { publication, participant },
+        });
+      });
+    });
+  }, [participants, prevParticipants]);
 
+  // when there is a new room...
+  useEffect(() => {
+    if (!_room) return;
+
+    // notify connected
+    dispatch({
+      type: ActionType.OnConnect,
+      payload: { room: _room },
+    });
+
+    // add event handlers
     const handlers = mapValues(
       HANDLER_ACTION_CREATORS,
       creator => (...args: any[]) => dispatch(creator(...args)),
     );
+    _room.setMaxListeners(Object.keys(handlers).length);
+    Object.keys(handlers).forEach(event => _room.on(event, handlers[event]));
 
-    console.log(`connected to room ${room.sid}`);
-
-    room.participants.forEach(participant =>
+    // forward participant connected events
+    _room.participants.forEach(participant =>
       dispatch({
         type: ActionType.OnParticipantConnected,
         payload: { participant },
       }),
     );
-    Object.keys(handlers).forEach(event => room.on(event, handlers[event]));
 
     return () => {
-      console.log(`disconnecting from room ${room.sid}`);
-      Object.keys(handlers).forEach(event => room.off(event, handlers[event]));
-      room.disconnect();
+      // on dispose
+
+      // remove all handlers
+      Object.keys(handlers).forEach(event => _room.off(event, handlers[event]));
+
+      // notify disconnected
+      dispatch({
+        type: ActionType.OnDisconnect,
+        payload: { room: _room },
+      });
+
+      // disconnect from twilio signalling
+      console.log(`[twilio] DISconnecting from room ${_room.sid}`);
+      _room.disconnect();
     };
-  }, [room]);
-
-  // connect to the room when available
-  useEffect(() => {
-    let mounted = true;
-    if (canConnect) {
-      (async () => {
-        const room = await connect(
-          token,
-          { name: id, audio: false, video: false },
-          // ^ join with nothing because we're going to mute/unmute ourselves later
-        );
-        if (!mounted) return;
-        dispatch({ type: ActionType.OnConnect, payload: { room } });
-      })();
-    }
-    return () => (mounted = false);
-  }, [canConnect, id, token]);
-
-  // auto-connect existing participant tracks
-  // useEffect(() => {
-  //   participants.forEach(participant => {
-  //     participant.tracks.forEach(publication =>
-  //       dispatch({
-  //         type: ActionType.OnTrackPublished,
-  //         payload: { publication, participant },
-  //       }),
-  //     );
-  //   });
-  // }, [participants]);
+  }, [_room]);
 
   // handle mute status
   useEffect(() => {
-    if (room) {
-      if (selfMuted) {
-        console.log(`[local] UNpublishing self track`);
-        room.localParticipant.unpublishTracks(tracks);
-      } else {
-        console.log(`[local] publishing self track`);
-        room.localParticipant.publishTracks(tracks);
-      }
-    }
-  }, [selfMuted, room, tracks]);
+    if (!_room) return;
 
-  // handle remote muting
+    if (selfMuted) {
+      console.log(`[local] UNpublishing self track`);
+      _room.localParticipant.unpublishTracks(tracks);
+    } else {
+      console.log(`[local] publishing self track`);
+      _room.localParticipant.publishTracks(tracks);
+    }
+
+    return () => {
+      _room.localParticipant.unpublishTracks(tracks);
+    };
+  }, [_room, selfMuted, tracks]);
+
+  // sync track loading to muting behavior
   // TODO: use twilio-video v2 trackSubscriptions to mute instead of local detaching
   useEffect(() => {
     // every time participants or subscribedTracks changes,
@@ -477,12 +510,46 @@ export default function useTwilioRoom(
   ]);
 
   // disconnect from room on unload
-  const handleBeforeUnload = useCallback(() => room.disconnect(), [room]);
-
   useEffect(() => {
-    window.addEventListener("beforeunload", handleBeforeUnload);
-    return () => window.removeEventListener("beforeunload", handleBeforeUnload);
-  }, [handleBeforeUnload]);
+    if (!_room) return;
+
+    const handler = () => _room.disconnect();
+    window.addEventListener("beforeunload", handler);
+    return () => window.removeEventListener("beforeunload", handler);
+  }, [_room]);
+
+  // connect to the room when available
+  // (this should only really change when `id` changes or `tracks` become available)
+  useEffect(() => {
+    let mounted = true;
+
+    // if the ability to connect to twilio has changed, nuke the current room
+    if (!id || !token) {
+      return _setRoom(undefined);
+    }
+
+    // if we can connect to twilio, let's do that in the background and then set the room
+    (async () => {
+      console.log(`[twilio] connecting to room ${id}`);
+      const room = await connect(
+        token,
+        { name: id, audio: false, video: false },
+        // ^ join with nothing because we're going to mute/unmute ourselves later
+      );
+
+      if (!mounted) {
+        // we've unmounted in the meantime, so disconnect
+        // and move along (move along like i know you do)
+        room.disconnect();
+        return;
+      }
+      _setRoom(room);
+    })();
+
+    return () => (mounted = false);
+  }, [id, token]);
+
+  // derived state for UI
 
   const setSelfMuted = useCallback(
     (selfMuted: boolean) =>
