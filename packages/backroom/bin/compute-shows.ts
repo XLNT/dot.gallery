ad2045559c104e8932af76f5c59f004b4b7e358f
Matt Condon (shrugs) <mattgcondon@gmail.com>
2019-09-14 00:23:50 +0200

    fix: compute shows from 2 am local time

--- packages/backroom/bin/compute-shows.ts
+++ packages/backroom/bin/compute-shows.ts
@@ -7,7 +7,7 @@ const NUM_SHOWS = 14;
 const main = async () => {
   const now = DateTime.fromISO(process.argv[2])
     .setZone("Europe/Berlin")
-    .set({ hour: 10 });
+    .set({ hour: 2 });
   // shows last 4 hours with 4 hour blocks inbetween, resetting at the beginning of the next day
   for (let i = 0; i < NUM_SHOWS; i++) {
     const start = now.plus({ hours: i * 8 });
